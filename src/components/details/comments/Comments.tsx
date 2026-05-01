8dbecbc8d50cbdc32d7a7ae2bc16c61d589aea19
ankit-2210 <78948475+ankit-2210@users.noreply.github.com>
2026-05-01 13:26:10 +0530

    updated

--- src/components/details/comments/Comments.tsx
+++ src/components/details/comments/Comments.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../../redux/hooks";
 import Comment from "./Comment";
 import Swal from "sweetalert2";
